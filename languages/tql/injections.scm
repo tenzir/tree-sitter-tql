ed00aee32c31c40aeb47d8ea37802cb73b6c01eb
Matthias Vallentin <matthias@vallentin.net>
2025-09-22 08:25:27 +0200

    Fix Zed injection syntax

--- languages/tql/injections.scm
+++ languages/tql/injections.scm
@@ -1,6 +1,6 @@
 ;; Highlight YAML frontmatter using the YAML grammar.
 
 (frontmatter
-  body: (frontmatter_body) @injection.content)
-(#set! injection.language "yaml")
-(#set! injection.combined)
+  body: (frontmatter_body) @injection.content
+  (#set! injection.language "yaml")
+  (#set! injection.combined))
